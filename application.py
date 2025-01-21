d25830c26e5025a025c18c4055985b04c66b095c
ViniciusMihael <viniciusmihael2@gmail.com>
2025-01-21 01:50:47 -0300

    mudando os comandos

--- application.py
+++ application.py
@@ -1,12 +1,7 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-
-def read_root():
-    return{"Hello":"Word"}
-
+@app.get('/')
+def get_languagens():
+    return{"sucess":"foi"}
